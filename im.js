2f4bd0687f5c1702f4fbb3f84e03c9ed8b18cd2e
madhu2904 <madhumithavelusamy0@outlook.com>
2024-04-10 12:54:56 +0530

    final commit after all changes

--- im.js
+++
@@ -1,6 +0,0 @@
-fetch('http://programming-quotes-api.herokuapp.com/quotes/random')
-.then(response=>{
-return response.json();
-})
-.then(data => {console.log(data)})
-.catch(error => console.log(error))
--- index.js
+++ index.js
@@ -1,26 +1,27 @@
- getquote=async ()=>
-{
-    var url= "https://type.fit/api/quotes"
-const response = await fetch(url);
-let AllQuotes =await response.json();
 
-}
+const quoteText=document.getElementById("quoteText");
+const author=document.getElementById("authorText");
+const linkedinButton=document.getElementById("linkedin");
+const button=document.getElementById("btn");
 
 
-
-fetch("https://type.fit/api/quotes")
-.then(response=>
+const getQuote= async ()=>{
+    var url="https://type.fit/api/quotes";
+    const response=await fetch(url);
+    if(!response.ok)
     {
-        if(!response.ok){
-            throw new Error("there is an error in loading")
-        }
-        
-        return response.json()
-    })
-.then(data => {c=([data[0].text,data[1].author]);
-    
-    document.getElementById("quoteBox").innerHTML
-})
-.catch(error => console.log(error))
+        console.log("error");
 
-
+    }
+    const Allquotes=await response.json();
+    const index = Math.floor(Math.random()*Allquotes.length);
+    var quote=Allquotes[index].text;
+    var auth=Allquotes[index].author;
+    if(auth==null)
+    {
+        auth="Anonymous";
+    }
+    quoteText.innerHTML=quote;
+    author.innerHTML=auth;
+}
+getQuote();
